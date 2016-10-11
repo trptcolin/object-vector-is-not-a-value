91f5c9f0500fdcbf7ee293872d50501187bdc0c6
Colin Jones <trptcolin@gmail.com>
2016-10-11 16:11:26 -0500

    Add notes about workarounds

--- src/main/scala/hw.scala
+++ src/main/scala/hw.scala
@@ -1,7 +1,17 @@
+// adding an actual package here makes things work
+// package sup
+
 object Hi {
   def main(args: Array[String]) = {
     println("ohai!")
+
+    // fails to compile (on my machine)
     val xs = Vector(1,2,3)
+
+    // adding package-qualification instead makes things work
+    //val xs = scala.collection.immutable.Vector(1,2,3)
+
+
     println("kthxbai!")
   }
 }
